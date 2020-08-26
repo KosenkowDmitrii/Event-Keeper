44766cab314fd2038347e98de155877302573405
Dmitrii <kosenkowdima@gmail.com>
2020-08-26 19:37:50 +0300

    seeds db

--- db/seeder.js
+++ db/seeder.js
@@ -1,11 +1,19 @@
 // const mongoose = require('mongoose');
 // const Day = require('../models/day');
 
-// mongoose
-// .connect('mongodb://localhost/eventKeeper', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
-//   .then(() => {
-//   Day.insertMany
-//   }
+// mongoose.connect('mongodb://localhost/eventKeeper', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// const event_1 = new Day({
+//   date: '01.02',
+//   events: ['День рождения Хамона'],
+//   notes: ['Купить шарики'],
+// });
+
+// const event_2 = new Day({
+//   date: '03.02',
+//   events: ['Зубы'],
+//   notes: ['Не есть 2 часа'],
+// });
+
+// event_1.save();
+// event_2.save();
