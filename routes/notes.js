329e8321d9ba5b5dafac833a9f1e37bade9eb9f2
alinamiryuk <alinamiryuk@mail.ru>
2020-08-28 13:34:37 +0300

    notes bootstrap

--- routes/notes.js
+++ routes/notes.js
@@ -4,29 +4,30 @@ const Day = require('../models/day');
 
 router.get('/', async (req, res) => {
   let daysFind = await Day.find();
-let days = daysFind.reduce((acc, el) => {
-  let dateKey = el.date[0];
-  if(acc[dateKey]) {
-    acc[dateKey] = [...acc[dateKey], ... el.notes];
-    return acc;
-  } else {
-    acc[dateKey] = el.notes;
-    return acc;
-  }
-}, {})
+  console.log(daysFind);
+  let days = daysFind.reduce((acc, el) => {
+    let dateKey = el.date[0];
+    if (acc[dateKey]) {
+      acc[dateKey] = [...acc[dateKey], ...el.notes];
+      return acc;
+    } else {
+      acc[dateKey] = el.notes;
+      return acc;
+    }
+  }, {});
 
-let keys = Object.keys(days);
+  let keys = Object.keys(days);
 
-let data = keys.map(key => {
-  let res = {}
-  res.date = key
-  res.event = days[key]
-  return res
-})
-console.log(data);
-  
-    res.render('notes', { data });
+  let data = keys.map((key) => {
+    let res = {};
+    res.date = key;
+    res.event = days[key];
+    if (days[key][0] !== '') {
+      return res;
+    }
   });
 
+  res.render('notes', { data });
+});
 
 module.exports = router;
