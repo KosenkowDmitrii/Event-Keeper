941e5ad5478839519a5a7e6dec907b5658d40391
Dmitrii <kosenkowdima@gmail.com>
2020-08-28 11:16:35 +0300

    render main

--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,9 @@
 const form = document.querySelector('#createEventForm');
 
 
-form.addEventListener('submit', async (e) => {
+form.addEventListener('submit', async (e) => { //Логика eventAdd Добавить новое событие
   e.preventDefault();
   const { date, events, notes, action, method } = e.target;
-    console.log(e.target);
   const response = await fetch(action, {
     method,
     headers: {
@@ -16,11 +15,19 @@ form.addEventListener('submit', async (e) => {
       notes: notes.value,
     })
   })
+  window.location.reload();
   const result = await response.json();
 })
 
-const backBtn = document.getElementById('backBtn');
+const backBtn = document.getElementById('backBtn'); //кнопка назад на главную
 backBtn.addEventListener('click', function () {
   window.location = '/main';
 });
+
+const saveBtn = document.getElementById('saveBtn'); //кнопка релоада страницы при сохранении нового события
+saveBtn.addEventListener('click', function () {
+  window.location.reload();
+});
+
+
 
